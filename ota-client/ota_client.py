a3e77ebccfd3d969b6286d7f5f3a7bf77f30656c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-01-02 14:08:37 +0300

    ota-client: Implement "ota" command.
    Sends prepared ("canned") OTA image.

--- ota-client/ota_client.py
+++ ota-client/ota_client.py
@@ -18,6 +18,7 @@ AES_KEY = b"\x01" * 16
 rsa_key = None
 last_aes_key = None
 last_seq = 0
+rexmit = 0
 
 
 def add_digest(pkt):
@@ -47,6 +48,38 @@ def decode_pkt(pkt):
     return aes.decrypt(pkt)
 
 
+def send_recv(s, pkt):
+    global rexmit
+
+    while True:
+        try:
+            print("Sending #%d" % last_seq)
+            s.send(pkt)
+            resp = s.recv(1024)
+            #print("resp:", resp, len(resp))
+            resp_seq = struct.unpack("<I", resp[:4])[0]
+            if resp_seq != last_seq:
+                continue
+            resp = resp[4:]
+            resp = decode_pkt(resp)
+            #print("decoded resp:", resp)
+            resp_op, resp_len, resp_off = struct.unpack("<HHI", resp[:8])
+            print("resp:", (resp_seq, resp_op, resp_len, resp_off))
+            if resp_off != offset or resp_len != len(chunk):
+                print("Invalid resp")
+                continue
+            break
+        except socket.timeout:
+            print("timeout")
+            rexmit += 1
+
+def send_ota_end(s):
+    # Repeat few times to minimize chance of being lost
+    for i in range(3):
+        pkt = add_digest(struct.pack("<HHI", 0, 0, 0))
+        s.send(pkt)
+        time.sleep(0.1)
+
 def live_ota():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((args.address, 8266))
@@ -54,42 +87,17 @@ def live_ota():
 
     offset = 0
     with open(args.file, "rb") as f:
-        rexmit = 0
         while True:
             chunk = f.read(BLK_SIZE)
             if not chunk:
                 break
             pkt = make_pkt(offset, chunk)
-    #        print("pkt:", pkt)
-            while 1:
-                try:
-                    print("Sending #%d" % last_seq)
-                    s.send(pkt)
-                    resp = s.recv(1024)
-    #                print("resp:", resp, len(resp))
-                    resp_seq = struct.unpack("<I", resp[:4])[0]
-                    if resp_seq != last_seq:
-                        continue
-                    resp = resp[4:]
-                    resp = decode_pkt(resp)
-    #                print("decoded resp:", resp)
-                    resp_op, resp_len, resp_off = struct.unpack("<HHI", resp[:8])
-                    print("resp:", (resp_seq, resp_op, resp_len, resp_off))
-                    if resp_off != offset or resp_len != len(chunk):
-                        print("Invalid resp")
-                        continue
-                    break
-                except socket.timeout:
-                    print("timeout")
-                    rexmit += 1
+            #print("pkt:", pkt)
+            send_recv(s, pkt)
             offset += len(chunk)
-        # Repeat few times to minimize chance of being lost
-        for i in range(3):
-            pkt = add_digest(struct.pack("<HHI", 0, 0, 0))
-            s.send(pkt)
-            time.sleep(0.1)
 
-        print("Done, rexmits: %d" % rexmit)
+    send_ota_end(s)
+    print("Done, rexmits: %d" % rexmit)
 
 
 def make_ota():
@@ -134,6 +142,28 @@ def validate_ota(fname):
             cmd_parser.error("Invalid OTA file checksum, file corrupted")
 
 
+def canned_ota(fname):
+    validate_ota(fname)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((args.address, 8266))
+    s.settimeout(0.3)
+
+    with open(fname, "rb") as f_in:
+        # Skip signature
+        sig = f_in.read(10)
+        while True:
+            data = f_in.read(2)
+            sz = struct.unpack("<H", data)[0]
+            if not sz:
+                break
+            data = f_in.read(sz)
+            send_recv(s, data)
+
+    send_ota_end(s)
+    print("Done, rexmits: %d" % rexmit)
+
+
 cmd_parser = argparse.ArgumentParser(description="yaota8266 (yet another esp8266 OTA) client")
 cmd_parser.add_argument("command", help="ota/sign/live-ota")
 cmd_parser.add_argument("file", help="file to process")
@@ -148,6 +178,6 @@ if args.command == "live-ota":
 elif args.command == "sign":
     make_ota()
 elif args.command == "ota":
-    validate_ota(args.file)
+    canned_ota(args.file)
 else:
     cmd_parser.error("Unknown command")
