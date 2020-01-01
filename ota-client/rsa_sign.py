a483e3043cd32df5482f3d4554a4c9ec0c2a3820
JensDiemer <git@jensdiemer.de>
2020-01-01 17:30:25 +0100

    rsa_sign.py: Use Path() and subprocess and change " -> '

--- ota-client/rsa_sign.py
+++ ota-client/rsa_sign.py
@@ -1,60 +1,85 @@
 #!/usr/bin/env python3
 
-import os
+import subprocess
 from binascii import unhexlify
+from pathlib import Path
+
+RSA_PRIV_KEY = 'priv.key'
 
 
 def load_key():
-    comps = {}
+    cwd = Path(__file__).parent.resolve()
+    if not Path(cwd, RSA_PRIV_KEY).is_file():
+        raise FileNotFoundError('RSA key file not found in %s' % cwd)
 
-    for l in os.popen("openssl pkey -in priv.key -text", "r"):
-        l = l.rstrip()
-        if l[0] != " " and l[-1] == ":":
-            last_comp = l[:-1]
-        elif l.startswith("    "):
-            comps[last_comp] = comps.get(last_comp, "") + l.lstrip()
+    output = subprocess.check_output(
+        ['openssl', 'pkey', '-in', RSA_PRIV_KEY, '-text'],
+        universal_newlines=True,
+        cwd=str(cwd)
+    )
+    # print(output)
+
+    comps = {}
+    last_comp = None
+    for line in output.splitlines():
+        if line[0] != ' ' and line[-1] == ':':
+            last_comp = line[:-1]
+        elif line.startswith('    '):
+            comps[last_comp] = comps.get(last_comp, "") + line.strip()
 
     # print(comps)
+
+    assert 'modulus' in comps, f'modulus not found in: {comps!r}'
+    assert 'privateExponent' in comps, f'privateExponent not found in: {comps!r}'
+
     return comps
 
 
 def dump_modulus(comps):
     print('Copy&paste this RSA modulus line into your config.h:')
     print('-' * 100)
-    print('#define MODULUS "%s"' % comps["modulus"][2:].replace(":", "\\x"))
+    print('#define MODULUS %s' % comps['modulus'][2:].replace(':', '\\x'))
     print('-' * 100)
 
 
 def dump_exponent(comps):
-    print('pe = "%s"' % comps["privateExponent"][2:].replace(":", "\\x"))
+    print('pe = %s' % comps['privateExponent'][2:].replace(':', '\\x'))
 
 
 def sign(comps, to_sign):
-    mod = comps["modulus"]
-    assert mod.startswith("00:")
-    mod = mod[3:].replace(":", "")
+    mod = comps['modulus']
+    assert mod.startswith('00:')
+    mod = mod[3:].replace(':', "")
     BITS = len(mod) // 2 * 8
-#    print("Key bits:", BITS)
+#    print('Key bits:', BITS)
 
-    mod = int.from_bytes(unhexlify(mod), "big")
-    pe = int.from_bytes(unhexlify(comps["privateExponent"].replace(":", "")), "big")
+    mod = int.from_bytes(unhexlify(mod), 'big')
+    pe = int.from_bytes(unhexlify(comps['privateExponent'].replace(':', "")), 'big')
 
     pad_len = BITS // 8 - len(to_sign) - 3
-#    print("Pad length:", pad_len)
+#    print('Pad length:', pad_len)
     assert pad_len >= 8
 
-    buf = b"\0\x01" + (b"\xff" * pad_len) + b"\0" + to_sign
-#    print("Padded to-sign len:", len(buf))
-    val = int.from_bytes(buf, "big")
-#    print("Padded to-sign:", val, hex(val))
+    buf = b'\0\x01' + (b'\xff' * pad_len) + b'\0' + to_sign
+#    print('Padded to-sign len:', len(buf))
+    val = int.from_bytes(buf, 'big')
+#    print('Padded to-sign:', val, hex(val))
 
     sig = pow(val, pe, mod)
-#    print("Sig (int):", sig, hex(sig))
-    sig_b = sig.to_bytes(BITS // 8, "big")
+#    print('Sig (int):', sig, hex(sig))
+    sig_b = sig.to_bytes(BITS // 8, 'big')
 #    print(hexlify(sig_b))
     return sig_b
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     comps = load_key()
-    sign(comps, b"foob\0")
+
+    dump_modulus(comps)
+
+    print('\nprivateExponent:')
+    dump_exponent(comps)
+
+    to_sign = b'foob\0'
+    print(f'\nsigned {to_sign!r}:')
+    print(sign(comps, to_sign))
